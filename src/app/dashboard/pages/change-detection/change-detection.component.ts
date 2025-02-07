7d5728e44a73dbf12aa77ffcb0987aa7d5960259
Johan10Robayo <johan.robayo@gmail.com>
2025-02-06 21:36:29 -0500

    change detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component,computed,signal } from '@angular/core';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-change-detection',
   standalone: true,
-  imports: [],
-  templateUrl: './change-detection.component.html',
-  styleUrl: './change-detection.component.css'
+  imports: [TitleComponent, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template : `
+  <app-title [title]="currentFramework()" />
+  <pre>{{frameworkAsSignal() | json}}</pre>
+  <pre>{{frameworkAsProperty | json}}</pre>
+
+  `
 })
 export default class ChangeDetectionComponent {
+public currentFramework = computed(
+  ()=> `change - change detection = ${this.frameworkAsSignal().name}`
+);
 
+  public frameworkAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016
+  });
+
+  public frameworkAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016
+  }; 
+
+  /**
+   *
+   */
+  constructor() {
+    setTimeout(()=>{
+      // this.frameworkAsProperty.name = 'React';
+      this.frameworkAsSignal.update(value =>({
+        ...value,
+        name:'React'
+
+      }));
+      console.log("hecho")
+    },3000);
+  }
 }
--- src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,19 +1,25 @@
 import { CommonModule } from '@angular/common';
+import { ThisReceiver } from '@angular/compiler';
 import { Component, signal } from '@angular/core';
-
+import { TitleComponent } from '@shared/title/title.component';
+type Grade = 'A'|'B'|'F';
 @Component({
   selector: 'app-control-flow',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,TitleComponent],
   templateUrl: './control-flow.component.html',
   styleUrl: './control-flow.component.css'
 })
 export  default class ControlFlowComponent {
   public showContent = signal(false);
+  public grade = signal<Grade>('A');
+  public  frameworks = signal(['Angular','Vue','Svelte','Qwik','React','Blazor']);
+  public  frameworks2 = signal([]);
 
   public toggleContent()
   {
 
       this.showContent.update( value => !value);
+      
   }
 }
