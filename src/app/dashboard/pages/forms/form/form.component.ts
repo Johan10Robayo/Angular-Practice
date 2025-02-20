daeeff2fbe45c64cf535fb7d773ae9d2868a9c07
Johan10Robayo <johan.robayo@gmail.com>
2025-02-19 22:37:19 -0500

    reactive forms

--- src/app/dashboard/pages/forms/form/form.component.ts
+++ src/app/dashboard/pages/forms/form/form.component.ts
@@ -1,13 +1,29 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule, JsonPipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TitleComponent } from '@shared/title/title.component';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TitleComponent, ReactiveFormsModule,JsonPipe],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export default class FormComponent {
+  public form = signal<FormGroup>(
+    new FormGroup(
+      {
+        name: new FormControl('',[Validators.required,Validators.minLength(2)]),
+        tel: new FormControl(''),
+        email: new FormControl(''),
+        url: new FormControl(''),
+        sector: new FormControl(''),
+        langs: new FormControl(['javascript']),
+        employeesCount: new FormControl(0)
 
+
+      }
+    )
+  );//señal tipada
 }
